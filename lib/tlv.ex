072092064efa847f6b0a47e637df16d34dc4b93e
Michele Balistreri <opensource@bitgamma.com>
2016-03-23 09:42:01 +0300

    added typespecs

--- lib/tlv.ex
+++ lib/tlv.ex
@@ -2,9 +2,15 @@ defmodule TLV do
   @moduledoc """
   Provides functions to decode, encode and work with ASN.1 BER-TLV structures. It supports tags encoded on multiple
   bytes, length encoded on multiple bytes, indefinite length and both primitive and constructed TLVs.
-
   """
 
+  @typedoc """
+  Represents a BER-TLV structure. The `tag` is represented as a number with header-bits not separated from the tag.
+  This is to accomodate common usages of the encoding. Primitive TLVs have a binary `value`, while constructed have
+  a list of TLVs. The `indefinite_length` field is set by the decoder to reflect the original byte representation and
+  also serves as instruction to the encoder on how to encode the length.
+  """
+  @type t :: %TLV{tag: integer, value: binary | [TLV.t], indefinite_length: boolean}
   defstruct [:tag, :value, indefinite_length: false]
 
   @doc """
@@ -22,10 +28,12 @@ defmodule TLV do
         [%TLV{indefinite_length: false, tag: 0x80, value: <<0xAA>>}]
       }, ""}
   """
+  @spec decode(binary) :: {TLV.t, binary}
   defdelegate decode(tlv), to: TLVDecoder
 
   @doc """
   TODO
   """
+  @spec decode(TLV.t) :: binary
   defdelegate encode(tlv), to: TLVEncoder
 end
