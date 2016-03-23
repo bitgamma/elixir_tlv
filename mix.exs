fc14098b770861bee4649ac75560dbb32d9e5945
Michele Balistreri <opensource@bitgamma.com>
2016-03-23 14:35:46 +0300

    release version 0.1.0

--- mix.exs
+++ mix.exs
@@ -5,31 +5,29 @@ defmodule Tlv.Mixfile do
     [app: :tlv,
      version: "0.1.0",
      elixir: "~> 1.2",
+     description: "Encodes/Decodes BER-TLVs structures",
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:ex_doc, "~> 0.10", only: :dev},
       {:earmark, "~> 0.1", only: :dev},
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Michele Balistreri"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/bitgamma/elixir_tlv"}
     ]
   end
 end
